ef16e6a90a8368dec3081d81977641ac6579f585
KIM BEOM SU <87904384+nosibi@users.noreply.github.com>
2025-02-16 21:54:20 +0900

    Add files via upload

---
+++ multipleArray/Solution.java
@@ -0,0 +1,17 @@
+package multipleArray;
+
+public class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = {};
+        answer = new int[arr1.length][arr2[0].length];
+        for(int i = 0; i < answer.length; i++){
+            for(int j = 0; j < answer[i].length; j++){
+                answer[i][j] = 0;
+                for(int k = 0; k < arr2.length; k++){
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+}
